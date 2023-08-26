50f991d72f5edb594eb8fd4933e4a7a50f81e492
shhdeveloper <shhdeveloper@gmail.com>
2023-08-26 11:14:59 +0930

    s

---
+++ Scripts/Path.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class Path : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0;i<transform.childCount;i++)
+        {
+            transform.GetChild(i).name = "Point"+i;
+            if(i>0)
+            {
+                Debug.DrawLine(transform.GetChild(i).position,transform.GetChild(i-1).position,Color.yellow);
+                //transform.GetChild(i).LookAt(transform.GetChild(i-1).position);
+                
+            }
+        }
+    }
+}
---
+++ villagerMovementController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class villagerMovementController : MonoBehaviour
+{
+    public NavMeshAgent nav;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
